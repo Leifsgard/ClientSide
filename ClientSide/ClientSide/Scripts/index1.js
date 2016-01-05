21348453c0abb1647a7d62f1596f6327751f4815
Rasmus Leifsgård <Leifsgard@outlook.com>
2016-01-05 12:03:16 +0100

    Exercise 1.1 is döne!

---
+++ ClientSide/ClientSide/Scripts/index1.js
@@ -0,0 +1,19 @@
+﻿
+
+document.getElementById('btn').addEventListener('click', calculate, false);
+
+
+function calculate() {
+  
+    var nb1 = document.getElementById('nb1').value;
+    var nb2 = document.getElementById('nb2').value;
+
+    if (nb1 > nb2) {
+        document.getElementById('nb3').innerHTML = nb1;
+    }
+   else if (nb1 < nb2) {
+        document.getElementById('nb3').innerHTML = nb2;
+    } else {
+       document.getElementById('nb3').innerHTML = "You entered the same number";
+    }
+}
