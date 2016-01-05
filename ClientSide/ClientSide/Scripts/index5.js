299a4ebb3c6d20f08bd5fd317ab7a40c8fe89950
Rasmus Leifsgård <Leifsgard@outlook.com>
2016-01-05 13:53:24 +0100

    Exercise 5

---
+++ ClientSide/ClientSide/Scripts/index5.js
@@ -0,0 +1,11 @@
+﻿
+document.getElementById('Omvandla').addEventListener('click', calculate, false);
+
+function calculate() {
+
+    var kronor = document.getElementById('pengar').value;
+    var dollar = (kronor * 6);
+    var pund = (kronor * 12);
+    document.getElementById('Dollar').innerHTML = dollar;
+    document.getElementById('Pund').innerHTML = pund;
+};
