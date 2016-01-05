162a75e35c2b3ecacd732bc95eb4735cd30fffa7
Rasmus Leifsgård <Leifsgard@outlook.com>
2016-01-05 12:17:37 +0100

    Lab 2

--- ClientSide/ClientSide/Scripts/index2.js
+++ ClientSide/ClientSide/Scripts/index2.js
@@ -1,1 +1,21 @@
-﻿
+﻿var exerciseThreeButton = document.getElementById("threeButton");
+var exerciseThreeClearButton = document.getElementById("threeButtonClear");
+
+exerciseThreeButton.addEventListener("click", function () {
+    runLoonTwoToHundredEvenNumbers();
+}, true);
+
+exerciseThreeClearButton.addEventListener("click", function () {
+    document.getElementById("exerciseThreeResult").innerHTML = "Väntar på körning...";
+}, true);
+
+function runLoonTwoToHundredEvenNumbers() {
+    var numbers = "Alla jämna tal " + "<br>";
+    for (var i = 1; i <= 100; i++) {
+        if (i % 2 === 0) {
+            numbers += " " + i;
+           
+        }
+    }
+    document.getElementById("exerciseThreeResult").innerHTML = numbers;
+};
