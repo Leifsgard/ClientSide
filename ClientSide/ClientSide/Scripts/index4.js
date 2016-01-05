6d614baf91a86f0928776641319e129d76cd6a99
Rasmus Leifsgård <Leifsgard@outlook.com>
2016-01-05 12:58:19 +0100

    Exercise fya

---
+++ ClientSide/ClientSide/Scripts/index4.js
@@ -0,0 +1,9 @@
+﻿var hourButton = document.getElementById('Calculation');
+
+hourButton.addEventListener('click', function () {
+    var hours = document.getElementById('Hours').value;
+    var minutes = (hours * 60);
+    var seconds = (minutes * 60);
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+}, true);
