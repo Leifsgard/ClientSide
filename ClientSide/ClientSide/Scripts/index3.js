647f7cc5a4444cbe034a08009f3b7f572dea41bb
Rasmus Leifsgård <Leifsgard@outlook.com>
2016-01-05 12:26:01 +0100

    Exercise 3 är döne också

---
+++ ClientSide/ClientSide/Scripts/index3.js
@@ -0,0 +1,8 @@
+﻿
+var salesButton = document.getElementById('buttonForSalesNumber');
+
+salesButton.addEventListener('click', function () {
+    var sales = document.getElementById('sales').value;
+    var result = (15000 + (sales * 0.09));
+    document.getElementById('result').innerHTML = result;
+}, true);
