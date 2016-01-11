73c9ef3ccc9c2c9cbfc24581a542d7a2ed666f45
Rasmus Leifsgård <Leifsgard@outlook.com>
2016-01-11 22:28:35 +0100

    Rättade till lite, tog ett tag.
    Kan inte dra in filer längre, vet inte varför så måste manuellt skriva
    sökvägen. Råkade skriva fel på en siffra, det tog sitt lilla tag att
    hitta. MEN NU KAN MAN SÖKA!

--- ClientSide/ClientSideExercise3/Scripts/index.js
+++ ClientSide/ClientSideExercise3/Scripts/index.js
@@ -1,1 +1,31 @@
-﻿
+﻿$(document).ready(function () {
+    $('#search').keyup(function () {
+        var searchField = $('#search').val();
+        var regex = new RegExp(searchField, 'i');
+
+        $.getJSON('products.json', function (data) {
+            var output = '<ul class="searchResults">';
+
+            $.each(data, function (key, val) {
+                if ((val.name.search(regex) !== -1 ||
+                    val.about.search(regex) !== -1)) {
+                    output += '<li>'
+                        + '<p>'
+                        + '<strong>'
+                        + val.name
+                        + '</strong>'
+                        + '</p>'
+                        + '<p>'
+                        + val.about
+                        + '</p>'
+                        + "<img src='Images/" + val.url_image + "'/>'"
+                        + '</li>';
+                }
+            });
+            output += "</ul>";
+            $('#productDiv').html(output);
+        });
+    });
+});
+
+
