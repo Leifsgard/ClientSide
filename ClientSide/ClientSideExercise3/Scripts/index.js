4471bf6a56fd821c4b22312691dfa80ee5c4c958
Rasmus Leifsgård <Leifsgard@outlook.com>
2016-01-11 22:45:28 +0100

    Finished!!!!

--- ClientSide/ClientSideExercise3/Scripts/index.js
+++ ClientSide/ClientSideExercise3/Scripts/index.js
@@ -26,6 +26,66 @@
             $('#productDiv').html(output);
         });
     });
+
+    $('.clickBox').on('click', function () {
+        var searchField = $('#search').val();
+        var regex = new RegExp(searchField, 'i');
+
+        $.getJSON('products.json', function (data) {
+            var output = '<ul class="searchResults">';
+
+            $.each(data, function (key, val) {
+                if ((val.name.search(regex) !== -1 ||
+                    val.about.search(regex) !== -1)) {
+                    if ($('#fullmenu').is(":checked")) {
+                        output += '<li>'
+                            + '<p>'
+                            + '<strong>'
+                            + '<span class="green">'
+                            + val.name
+                            + '</span>'
+                            + '</strong>'
+                            + '</p>'
+                            + '<p>'
+                            + '<strong class="color">price: </strong>'
+                            + '<span class="Orange">'
+                            + '$'
+                            + val.price
+                            + '</span>'
+                            + '<p>'
+                            + '<strong class="color">about: </strong>'
+                            + val.about
+                            + '</p>'
+                            + "<img src='Images/" + val.url_image + "'/>'"
+                            + '</li>';
+                    } else if ($('#' + val.type).is(":checked")) {
+                        output += '<li>'
+                            + '<p>'
+                            + '<strong>'
+                            + val.name
+                            + '</strong>'
+                            + '</p>'
+                             + '<p>'
+                            + '<strong class="color">price: </strong>'
+                            + '<span class="Blue">'
+                            + '$'
+                            + val.price
+                            + '</span>'
+                            + '<p>'
+                            + '<strong class="color">about: </strong>'
+                            + val.about
+                            + '</p>'
+                            + "<img src='Images/" + val.url_image + "'/>'"
+                            + '</li>'
+                        + '<p></br></p>'
+                        + '<p></br></p>';
+                    }
+                }
+            });
+            output += "</ul>";
+            $('#productDiv').html(output);
+        });
+    });
 });
 
 
